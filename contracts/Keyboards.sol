fdc673891cf57ba35fb17152b862ab6b7780e94f
Titusz Ban <tituszban@gmail.com>
2022-01-05 18:52:31 +0000

    Create basic contract

---
+++ contracts/Keyboards.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+contract Keyboards {
+  string[] public createdKeyboards;
+
+  function getKeyboards() view public returns(string[] memory) {
+    return createdKeyboards;
+  }
+}
